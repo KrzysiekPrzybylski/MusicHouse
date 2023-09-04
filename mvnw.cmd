64c592d97ff436fd4ff26b413e8d048a0ecde611
Krzysiek Przybylski <krzy.przybylski@gmail.com>
2023-09-04 02:21:14 +0200

    main mm

--- mvnw.cmd
+++ mvnw.cmd
@@ -46,6 +46,7 @@ if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 if not "%MAVEN_SKIP_RC%" == "" goto skipRcPre
 @REM check for pre script, once with legacy .bat ending and once with .cmd ending
 if exist "%USERPROFILE%\mavenrc_pre.bat" call "%USERPROFILE%\mavenrc_pre.bat" %*
+
 if exist "%USERPROFILE%\mavenrc_pre.cmd" call "%USERPROFILE%\mavenrc_pre.cmd" %*
 :skipRcPre
 
